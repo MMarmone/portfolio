d8787f2010b9065ecd9cb2405298cf6160e487f2
kushiim <michelmarmonemarini@gmail.com>
2023-12-27 14:34:51 +0100

    add vercel analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import SideBar from "./SideBar";
@@ -9,6 +10,8 @@ const Layout = ({ children }) => (
     <SideBar/>
     <Navbar />
     {children}
+    <Analytics />
+    <SpeedInsights/>
     <Footer />
     
   </>
